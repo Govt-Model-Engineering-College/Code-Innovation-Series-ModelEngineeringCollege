0a2fccf3c67b729a7bb0bd8d1778450e7f26bbab
Vishruth S <vs.vishruth.s@gmail.com>
2020-12-24 20:48:22 +0530

    :tada: Adding Team Cypher Project

---
+++ CYPHER/Cypher_Project-TeachEasy/public/js/drawing.js
@@ -0,0 +1,116 @@
+// 'use strict';
+
+(function () {
+
+    let socket = io();
+    let canvas = document.getElementsByClassName('whiteboard')[0];
+    let colors = document.getElementsByClassName('color');
+    let context = canvas.getContext('2d');
+
+    let current = {
+        color: 'black'
+    };
+    let drawing = false;
+
+    canvas.addEventListener('mousedown', onMouseDown, false);
+    canvas.addEventListener('mouseup', onMouseUp, false);
+    canvas.addEventListener('mouseout', onMouseUp, false);
+    canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+
+    canvas.addEventListener('touchstart', onMouseDown, false);
+    canvas.addEventListener('touchend', onMouseUp, false);
+    canvas.addEventListener('touchcancel', onMouseUp, false);
+    canvas.addEventListener('touchmove', throttle(onMouseMove, 10), false);
+
+    for (let i = 0; i < colors.length; i++) {
+        colors[i].addEventListener('click', onColorUpdate, false);
+    }
+
+    socket.on('drawing', onDrawingEvent);
+    window.addEventListener('resize', onResize, false);
+    onResize();
+
+
+    const drawLine = (x0, y0, x1, y1, color, emit) => {
+        context.beginPath();
+        context.moveTo(x0, y0);
+        context.lineTo(x1, y1);
+        context.strokeStyle = color;
+        context.lineWidth = color === "white" ? 20 : 2;
+        context.stroke();
+        context.closePath();
+
+        if (!emit) { return; }
+        let w = canvas.width;
+        let h = canvas.height;
+
+        socket.emit('drawing', {
+            x0: x0 / w,
+            y0: y0 / h,
+            x1: x1 / w,
+            y1: y1 / h,
+            color: color
+        });
+    }
+
+    function onMouseDown(e) {
+        drawing = true;
+        current.x = e.clientX || e.touches[0].clientX;
+        current.y = e.clientY || e.touches[0].clientY;
+    }
+
+    function onMouseUp(e) {
+        if (!drawing) { return; }
+        drawing = false;
+        drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+    }
+
+    function onMouseMove(e) {
+        if (!drawing) { return; }
+        drawLine(current.x, current.y, e.clientX || e.touches[0].clientX, e.clientY || e.touches[0].clientY, current.color, true);
+        current.x = e.clientX || e.touches[0].clientX;
+        current.y = e.clientY || e.touches[0].clientY;
+    }
+
+    function onColorUpdate(e) {
+        current.color = e.target.className.split(' ')[1];
+    }
+
+    function throttle(callback, delay) {
+        let previousCall = new Date().getTime();
+        return function () {
+            let time = new Date().getTime();
+
+            if ((time - previousCall) >= delay) {
+                previousCall = time;
+                callback.apply(null, arguments);
+            }
+        };
+    }
+
+    function onDrawingEvent(data) {
+
+        let w = canvas.width
+        let h = canvas.height;
+        drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    }
+
+    function onResize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+
+
+})();
+
+const enableDrawing = () => {
+    let mainVideos = document.getElementById('videoGrid')
+    if (!mainVideos.style.display)
+        mainVideos.style.display = "flex"
+    mainVideos.style.display = mainVideos.style.display === "flex" ? "none" : "flex"
+    let board = document.getElementById('DrawingBoard')
+    board.style.display = board.style.display === "block" ? "none" : "block"
+
+}
+
---
+++ CYPHER/Cypher_Project-TeachEasy/server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
+const { addUser, removeUser, getUsers } = require('./users')
+
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+})
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use('/peerjs', peerServer)
+app.get('/', (req, res) => {
+    res.render('home', { id: RandomIdGenerate(9) })
+})
+
+app.get('/:room', (req, res) => {
+    const name = req.query.name
+    res.render('room', { roomId: req.params.room, username: name })
+})
+
+
+app.post('/create', (req, res) => {
+    const name = req.body.name.trim()
+    const id = req.body.code.trim()
+    res.redirect(`/${id}?name=${name}`)
+})
+
+app.post('/join', (req, res) => {
+    const name = req.body.name.trim()
+    const id = req.body.code.trim()
+    res.redirect(`/${id}?name=${name}`)
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId, username) => {
+        addUser({ id: userId, name: username, room: roomId });
+
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId, username);
+
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message, username)
+        })
+
+        io.in(roomId).emit('users-in-room', getUsers())
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+            removeUser(userId)
+            io.in(roomId).emit('users-in-room', getUsers())
+        })
+    })
+    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data))
+
+})
+
+const RandomIdGenerate = (length) => {
+    const characters = 'abcdefghijklmnopqrstuvwxyz'
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 1; i <= length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+        i % 3 === 0 && i !== length ? result += "-" : null
+    }
+
+    return result;
+}
+
+server.listen(process.env.PORT || 3030, () => {
+    console.log("App running at http://localhost:3030")
+});  
